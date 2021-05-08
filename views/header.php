6f1f27812dbd1f7698bbe6ad1ae50367d500e746
Aulon1 <aulon.hamiti@student.uni-pr.edu>
2021-05-09 00:56:36 +0200

    home gets active class

--- views/header.php
+++ views/header.php
@@ -6,11 +6,11 @@ Portal</div>
         <i class="fas fa-bars"></i>
       </label>
       <ul>
-<li><a  href="portal.php">Home</a></li>
+<li><a class="active" href="portal.php">Home</a></li>
 <li><a href="Lajme.php">Lajme</a></li>
 <li><a href="Shëndetësia.php">Shëndetësia</a></li>
 <li><a href="Sport.php">Sport</a></li>
-<li><a class="active" href="Argëtim.php">Argëtim</a></li>
+<li><a  href="Argëtim.php">Argëtim</a></li>
 <li><a href="Kontakti.php">Kontakti</a></li>
 </ul>
 </nav>
