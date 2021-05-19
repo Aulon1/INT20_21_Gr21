f2cb5e8e4f4bc7035a32f152e1402a7e786188f4
besjanakurtaj <74364414+besjanakurtaj@users.noreply.github.com>
2021-05-19 14:53:45 +0200

    Ndryshimet ne addnews

--- Lajme.php
+++ Lajme.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    include 'autoload.php';
+    $news = new news;
+    $errors = [];
+    ?>
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -6,78 +12,199 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+
+<title>Lajmet</title>
 <style> 
 /**Gjersia e main, ne anen e majt **/
 main{
-    width: 75%;
+    width: 80%;
     float: left;
+    margin: 100 100 20 20;
+    margin: auto;
 }
-/**Gjersia e aside, ne anen e djatht **/
-aside{
-    width: 25%;
-    float: right; 
-}
-/**Gjersia e titullit, margjina, ngjyra, bakgrandi, kalimi i ngjyrave, rreshtimi i teksitit majtas, madhesia e teksit, hija e tekstit**/
-h1{ 
+nav{
+  display: flex;
+  height: 80px;
   width: 100%;
-  margin: auto ;
-  color: #000000;
-  background-color: white;
-  background-image: linear-gradient(white, gray);
-  text-align: left;
-  font-size: 40px;
-  text-shadow: 2px 2px 4px white;
+  background: #1b1b1b;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px 0 100px;
+  flex-wrap: wrap;
 }
 
 
+/* style per logon ky qasmi me nav ne clasen e loges */
+
+nav .logo{
+  color: #fff;
+  font-size: 35px;
+  font-weight: 600;
+}
 
 
-.vija{
+/*style i listave me an te navit */
+nav ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+}
+nav ul li{
+  margin: 0 5px;
+}
+
+/*style i linqeve brenda listave ku edhe kemi perdor transiton per te gjitha linqet brenda listav*/
+
+nav ul li a{
+  color: #f2f2f2;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 8px 15px;
+  border-radius: 5px;
+  letter-spacing: 1px;
+  transition: all 0.3s ease;
+}
+
+/*style i linkut te par me clasen aktive ku i kemi dhen ngjyr*/
+nav ul li a.active{
+  color: #111;
+  background: #fff;
+
+}
+/*style i linqeve me hover qe kur ti afrojm mausin marrin ngjyr linqet*/
+nav ul li a:hover{
+  color: #111;
+  background: #fff;
+}
+
+/*style pjesa e atij butonit kur e zvoglojm faqen qe shfaqet qe esht i mirrem nga google ajo figura*/
+nav .menu-btn i{
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+  display: none;
+}
+/*style i inputit me type checkbox*/
+input[type="checkbox"]{
+  display: none;
+}
+
+/*style per screen max te tabletav 1000px*/
+@media (max-width: 1000px){
+  nav{
+    padding: 0 20px 0 30px;
+  }
+}
+
+/*style per screen te telefonava se si duket ne telefona*/
+@media (max-width: 960px) {
+  nav .menu-btn i{
+    display: block;
+    }
+    /* style i butnit te menu qe kur zvoglohet hapet e pasta per ta mbyllur shfaq shejen e x */
+  #click:checked ~ .menu-btn i:before{
+    content: "\f00d";
+  }
+  nav ul{
+    position: fixed;
+    top: 80px;
+    left: -100%;
+    background: #111;
+    height: 100vh;
     width: 100%;
-    height: 6px;
-    background-color: black;
+    text-align: center;
+    display: block;
+    transition: all 0.3s ease;
+  }
+  #click:checked ~ ul{
+    left: 0;
+  }
+  nav ul li{
+    width: 100%;
+    margin: 40px 0;
+  }
+  nav ul li a{
+    width: 100%;
+    margin-left: -100%;
+    display: block;
+    font-size: 20px;
+    transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  }
+  #click:checked ~ ul li a{
+    margin-left: 0px;
+  }
+  nav ul li a.active{
+    background: none;
+    color: cyan;
+
+  }
+  nav ul li a:hover{
+    background: none;
+    color: cyan;
+  }
 }
-/**ngjyra e teksiti, hija, bakgrandi, madhesia e teksiti**/
+
 
 </style> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-
   <body>
-  <?php include('./views/header.php')  ?>
-<div class="LAJMET">
-  
-  <h1>Lajmet nga vendi</h1>
+  <nav>
+      <div class="logo">
+Portal</div>
+<input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+<li><a  href="portal.php">Home</a></li>
+<li><a class="active" href="Lajme.php">Lajme</a></li>
+<li><a href="Shëndetësia.php">Shëndetësia</a></li>
+<li><a href="Sport.php">Sport</a></li>
+<li><a  href="Argëtim.php">Argëtim</a></li>
+<li><a href="Kontakti.php">Kontakti</a></li>
+</ul>
+</nav>
+  <div class="LAJMET">
+
+
+  <br>
+
+
+  <h1>Lajmet e fundit</h1>
   <hr class="vija">
 <main>
-
+<div class="container my-5">
+        <div class="row">
+            <?php foreach($news->all() as $news): ?>
+                <div class="col-md-4 my-2">
+                            <?php
+                                $image = json_decode($news['image'], true);
+                        {
+                            ?>
+                                <img src="./image/<?= $image[0] ?>" alt="<?= $news['titulli'] ?>" class="img-fluid" style="height: 220px; display: block; margin: 0px auto;" />
+                                <?php }  ?>
+                            <h4><?= $news['titulli'] ?></h4>
+                            <p>
+                            <?= $news['permbajtja'] ?>
+                            </p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
         <a href="addnews.php" target="_blank">
-        <button>Shto Lajme</button></a>
-    </main>
-</div>
-<aside>
-  <br>
-  <!--Videot anash ne aside -->
-  <video width="320" height="240"  autoplay loop>
-    <source src="stay.mp4" type="video/mp4"> 
-  </video>  <hr class="vija" >
-  <br>
-  <video width="320" height="240" autoplay loop>
-    <source src="Covid.mp4" type="video/mp4">
-  </video>
-  <hr class="vija" >
-  <br>
-  <h3>Harta e shperndarjes se COVID-19</h3>
-  <video width="320" height="240" autoplay loop>
-    <source src="harta.mp4" type="video/mp4">
-  </video>
-  <hr class="vija" >
-</aside>
-</div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+       
+</main>
+</div> 
+<button  class="btn btn-dark btn-lg btn-block">Shto Lajme</button></a>
+
+  </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<?php include('./views/footer.php')  ?>
+
+
+    <?php include('./views/footer.php')  ?>
 </body>
 </html>
--- addnews.php
+++ addnews.php
@@ -1,17 +1,41 @@
 <?php
+session_start();
 include 'autoload.php';
+$n = new news; 
 $errors=[];
-$n= new news;
-$news=$n->all();
+$newss=$n->all();
+
+if(isset($_POST['save_news_btn'])) {
+  $titulli = $_POST['titulli'];
+  $permbajtja = $_POST['permbajtja'];
+ $image="";
+ $tmp_name= [];
+ 
+ if(!empty($titulli) && !empty($permbajtja) && !empty($_FILES['image']['name'])) {
+  $image = [$_FILES['image']['name']];
+  move_uploaded_file($_FILES['image']['tmp_name'], "./image/".$_FILES['image']['name']);
+
+  if($n->create(['titulli' => $titulli, 'permbajtja' => $permbajtja,  'image' => json_encode($image)]))
+      header("Location: addnews.php");
+  else
+      $errors[] = "Something want wrong while adding the product!";
+} else 
+  $errors[] = "All fields are required!";
+}
+
 if(isset($_GET['action']) && ($_GET['action'] == 'edit')) {
   if(isset($_GET['news_id']) && (is_numeric($_GET['news_id'])))  {
       echo "PO";
       $_SESSION['news_id'] = $_GET['news_id'];
-      header("Location: edit-product.php");
+      header("Location: edit_news.php");
   } else
       $errors[] = "Product doesn't exist!";
   
 }
+
+
+
+
 if(isset($_GET['action']) && ($_GET['action'] == 'delete')) {
   if(isset($_GET['news_id']) && (is_numeric($_GET['news_id'])))  {
       if($n->delete($_GET['news_id'])) 
@@ -35,7 +59,7 @@ if(isset($_GET['action']) && ($_GET['action'] == 'delete')) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    
     <title>Add News</title>
     <style>
       h3{
@@ -52,10 +76,22 @@ if(isset($_GET['action']) && ($_GET['action'] == 'delete')) {
     <div class="container my-5">
         <h3>Krijo lajmet</h3>
 <div class="row">
-
+<?php
+                if(count($errors)) {
+            ?>
+                <div class="alert alert-danger">
+                    <ul>
+                   <?php foreach($errors as $error): ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php   
+                }
+            ?>
     <div class="col-nd-8 ">
     <div class="container-fluid mt-4">
-<form action="data.php" method="post">
+<form enctype="multipart/form-data" method="post">
 <div class="form-group">
 <label for="titulli">Titulli:</label>
 <input type="text" name="titulli" id="titulli" placeholder="Titulli i lajmit..." class="form-control">
@@ -65,34 +101,41 @@ if(isset($_GET['action']) && ($_GET['action'] == 'delete')) {
 <textarea name="permbajtja" id="permbajtja" cols="50" rows="3" placeholder="Permbajtja e lajmit..."></textarea>
 </div>
 <div class="form-group">
-<input type="file" name="images[]" multiple class="form-control" id="image">
-</div>
+ <label for="image">Images</label>
+ <br />
+<input type="file" name="image" />
+                    </div>
 <div class="form-group">
-<button type="submit" class="btn btn-info btn-block">SHTO</button>
+<button type="submit" name="save_news_btn" class="btn btn-info btn-block">SHTO</button>
 </div>
 </form>
 </div>
 </div>
 </div>
 </div>
+<br>
 
 <div class="container my-5">
   <h3>Lajmet</h3>
   <div class="row">
 
     <div class="col-nd-8">
-    <?php if(count($news)>0):?>
+
+    <?php if(count($newss)>0):?>
       <div class="table-responsive">
-        <table class="table table-borderes">
+        <table class="table table-bordered">
           <tr>
             <th>ID</th>
             <th>Titulli</th>
+            <th>Data</th>
             <th></th>
           </tr>
-          <?php foreach($news as $news): ?>
+          <?php foreach($newss as $news): ?>
                             <tr>
                                 <th><?= $news['id'] ?></th>
                                 <th><?= $news['titulli'] ?></th>
+                                <th><?= $news['create_at'] ?></th>
+            
 
                                 <th>
                                     <a href="?action=edit&news_id=<?= $news['id'] ?>" class="btn btn-sm btn-primary">Edit</a>
@@ -116,7 +159,6 @@ if(isset($_GET['action']) && ($_GET['action'] == 'delete')) {
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 
 </body>
 </html>
